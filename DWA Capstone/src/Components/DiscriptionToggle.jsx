75b80ecda47d4f49c023a155c40c9402b557e499
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-08-03 13:26:34 +0200

    Added gitignore file

---
+++ DWA Capstone/src/Components/DiscriptionToggle.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const DescriptionToggle = ({ description }) => {
+  const [isMinimized, setIsMinimized] = useState(true);
+
+  const toggleDescription = () => {
+    setIsMinimized((prev) => !prev);
+  };
+
+  return (
+    <span className="description" onClick={toggleDescription}>
+      {isMinimized ? (
+        <>
+          {description.substr(0, 100)}
+          <button>Read More</button>
+        </>
+      ) : (
+        <>
+          {description}
+          <button>Read Less</button>
+        </>
+      )}
+    </span>
+  );
+};
+
+export default DescriptionToggle;
---
+++ DWA Capstone/src/Components/FavoriteButton.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+const FavoriteButton = () => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  // Function to handle marking/unmarking the podcast as a favorite
+  const handleFavoriteClick = () => {
+    setIsFavorite((prevFavorite) => !prevFavorite);
+  };
+
+  return (
+    <button onClick={handleFavoriteClick} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+      {isFavorite ? <FavoriteIcon color="secondary" /> : <FavoriteBorderIcon />}
+    </button>
+  );
+};
+
+export default FavoriteButton;
